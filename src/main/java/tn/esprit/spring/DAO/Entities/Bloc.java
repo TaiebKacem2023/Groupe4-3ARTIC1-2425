9043f5c79adf77f00321bc4da9b48a5e9bd5deb9
achref-hamzaou <houssem.ha@yahoo.com>
2025-06-04 13:10:33 +0100

    achref foyer

--- src/main/java/tn/esprit/spring/DAO/Entities/Bloc.java
+++ src/main/java/tn/esprit/spring/DAO/Entities/Bloc.java
@@ -1,28 +1,32 @@
 package tn.esprit.spring.DAO.Entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import java.io.Serializable;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "T_FOYER")
+@Table(name = "T_BLOC")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Foyer implements Serializable {
+public class Bloc{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long idFoyer;
-    String nomFoyer;
-    long capaciteFoyer;
-    @OneToOne(mappedBy = "foyer")
-    Universite universite;
-    @OneToMany(mappedBy = "foyer")
-    List<Bloc> blocs= new ArrayList<>();
+    long idBloc;
+    String nomBloc;
+    long capaciteBloc;
+    
+    @ManyToOne
+    @JsonIgnore
+    Foyer foyer;
+    @OneToMany(mappedBy = "bloc", fetch = FetchType.EAGER)
+    @JsonIgnore
+    List<Chambre> chambres= new ArrayList<>();
 }
--- src/main/java/tn/esprit/spring/DAO/Entities/Universite.java
+++ src/main/java/tn/esprit/spring/DAO/Entities/Universite.java
@@ -3,26 +3,24 @@ package tn.esprit.spring.DAO.Entities;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Table(name = "T_FOYER")
+@Table(name = "T_UNIVERSITE")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Foyer implements Serializable {
+public class Universite implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long idFoyer;
-    String nomFoyer;
-    long capaciteFoyer;
-    @OneToOne(mappedBy = "foyer")
-    Universite universite;
-    @OneToMany(mappedBy = "foyer")
-    List<Bloc> blocs= new ArrayList<>();
+    long idUniversite;
+    String nomUniversite;
+    String adresse;
+    @OneToOne(cascade = CascadeType.ALL) //ajout, Modif et suppression
+    Foyer foyer;
+
 }
