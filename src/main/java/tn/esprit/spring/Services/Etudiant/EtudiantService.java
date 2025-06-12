c691c94840081fcf155143f809321e3cbefcbb52
tahani cherif <cheriftahani92@gmail.com>
2025-06-12 11:13:57 +0100

    fix bug

--- src/main/java/tn/esprit/spring/Services/Etudiant/EtudiantService.java
+++ src/main/java/tn/esprit/spring/Services/Etudiant/EtudiantService.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring.Services.Etudiant;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import tn.esprit.spring.DAO.Entities.Etudiant;
@@ -27,7 +28,8 @@ public class EtudiantService implements IEtudiantService {
 
     @Override
     public Etudiant findById(long id) {
-        return repo.findById(id).get();
+        return repo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Etudiant not found with id: " + id));
     }
 
     @Override
