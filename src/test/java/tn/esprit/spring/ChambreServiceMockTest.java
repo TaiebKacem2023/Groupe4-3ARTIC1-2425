2caba42b0426280d4ae86342072d85761bf98182
Taieb KACEM <155002914+TaiebKacem2023@users.noreply.github.com>
2025-06-14 11:15:15 +0100

    Mock test updated

--- src/test/java/tn/esprit/spring/ChambreServiceMockTest.java
+++ src/test/java/tn/esprit/spring/ChambreServiceMockTest.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ChambreServiceTest {
+class ChambreMockTest {
 
     @Mock
     ChambreRepository chambreRepository;
